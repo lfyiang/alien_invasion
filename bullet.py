dfb1f55a745dfbe612d7d9cf0bbf0ea573743869
Crike <lfyiang@gmail.com>
2020-04-12 14:17:54 +0800

    Update bullet.py

--- bullet.py
+++ bullet.py
@@ -3,7 +3,7 @@ from pygame.sprite import Sprite
 
 
 class Bullet(Sprite):
-    """一个对飞船发射得子弹进行管理的类"""
+    """一个对飞船发射的子弹进行管理的类"""
 
     def __init__(self, ai_settings, screen, ship):
         """在飞船所示的位置创建一个子弹对象"""
